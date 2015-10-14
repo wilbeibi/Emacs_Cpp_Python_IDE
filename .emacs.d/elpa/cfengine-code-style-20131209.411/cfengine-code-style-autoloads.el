82b71ff4abe9620010c4579240f5b0163d0eb121
Hongyi Shen <wilbeibi@gmail.com>
2015-10-13 21:54:03 -0700

    fix C/C++ def jump back

---
+++ .emacs.d/elpa/cfengine-code-style-20131209.411/cfengine-code-style-autoloads.el
@@ -0,0 +1,16 @@
+;;; cfengine-code-style-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("cfengine-code-style.el") (22045 56558
+;;;;;;  929622 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; cfengine-code-style-autoloads.el ends here
---
+++ .emacs.d/elpa/cfengine-code-style-20131209.411/cfengine-code-style-pkg.el
@@ -0,0 +1,1 @@
+(define-package "cfengine-code-style" "20131209.411" "C code style for CFEngine project." 'nil :url "https://github.com/cfengine/core")
